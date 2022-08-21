4c9dc762e410301f55a64f0e21bd7a3b1d7039ed
zaid3tech <zaidiqbal49@gmail.com>
2022-08-21 23:56:57 +0500

    solution: integer to roman

---
+++ integer_to_roman/main.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def intToRoman(self, num):
+        symbol_map = dict(
+            [
+                ("I", 1),
+                ("IV", 4),
+                ("V", 5),
+                ("IX", 9),
+                ("X", 10),
+                ("XL", 40),
+                ("L", 50),
+                ("XC", 90),
+                ("C", 100),
+                ("CD", 400),
+                ("D", 500),
+                ("CM", 900),
+                ("M", 1000),
+            ]
+        )
+        #sort desc by value to choose the closest candiadate
+        symbol_map = dict( sorted(symbol_map.items(),
+                           key=lambda item: item[1],
+                           reverse=True)) 
+        output_roman = ""
+        while num > 0:
+            for key in symbol_map:
+                if num >= symbol_map[key]:
+                    num = num - symbol_map[key]
+                    output_roman = output_roman + key
+                    break 
+        return output_roman    
+            
+
+
+def main():
+    sol = Solution()
+    print(sol.intToRoman(58))
+
+
+if __name__ == "__main__":
+    main()
---
+++ integer_to_roman/problem.md
@@ -0,0 +1,19 @@
+### Integer to Roman
+
+  
+
+Given a roman numeral, convert it to an integer.
+
+  
+
+```
+
+Input: num = 1994
+
+Output: "MCMXCIV"
+
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+```
+
+[Original Problem Statement](https://leetcode.com/problems/integer-to-roman/)
